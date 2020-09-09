2387a094192fe916ec68bde28793b6076e8d1cdf
Hasan Ozovali <ozovalihasan@gmail.com>
2020-09-09 17:51:22 +0300

    Add a button to remove a book from library

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,28 @@
-const myBooks = document.querySelector('.book');
-const formButton = document.querySelector('.display-form');
-const formSlot = document.querySelector('.form-space');
-
-formButton.addEventListener('click', () => {
-  formSlot.style.display = 'block';
+const myBooks = document.querySelector(".book");
+const formButton = document.querySelector(".display-form");
+const formSlot = document.querySelector(".form-space");
+formButton.addEventListener("click", () => {
+  formSlot.style.display = "block";
 });
-const myLibrary = ['harry potter', 'hobbit'];
+const myLibrary = ["harry potter", "hobbit"];
 
 // function Book() {}
 
 // function addBookToLibrary() {}
 
 function showBook() {
-  let ourBooks = '';
-  myLibrary.forEach((book) => {
-    ourBooks += `<div> ${book} </div> `;
+  let ourBooks = "";
+  myLibrary.forEach((book, bookIndex) => {
+    ourBooks += `<div> ${book} </div>
+            <button onclick="removeBook(${bookIndex})" >Remove this book</button>
+    `;
   });
   myBooks.innerHTML = ourBooks;
 }
 
-showBook(myLibrary);
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  showBook();
+}
+
+showBook();
